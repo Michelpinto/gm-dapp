54e8c59ff12d1bbb8405820349e40ea1635eada8
Michel Pinto <michabbe9@live.com>
2022-02-19 14:30:37 +0100

    dapp done for now

---
+++ test/gm-test.js
@@ -0,0 +1,8 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("Gm", function () {
+    it("allows user to send eth to another address", async function () {
+        // To be done soon
+    });
+});
